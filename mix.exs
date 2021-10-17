9cc43acd8f474ea22f5cb41dfae99f4115c7842d
Kian-Meng, Ang <kianmeng@cpan.org>
2021-10-17 21:57:54 +0800

    Use and update correct ex_doc version

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule ImgProxy.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.24.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false}
     ]
   end
 end
